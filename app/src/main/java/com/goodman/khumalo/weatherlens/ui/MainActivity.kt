7a07032961ae74cd86fdc89733b185fe20f708b7
Goodman Khumalo <goodman.khumalo@standardbank.co.za>
2021-07-26 04:58:26 +0200

    removes hard coded coordinates

--- app/src/main/java/com/goodman/khumalo/weatherlens/ui/MainActivity.kt
+++ app/src/main/java/com/goodman/khumalo/weatherlens/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.goodman.khumalo.weatherlens.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -12,8 +15,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.goodman.khumalo.weatherlens.R
 import com.goodman.khumalo.weatherlens.model.*
 import com.goodman.khumalo.weatherlens.viewmodel.AccuWeatherViewModel
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationStr: String
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
     private lateinit var model: AccuWeatherModel
     private lateinit var viewModel: AccuWeatherViewModel
     private lateinit var cityName: TextView
@@ -31,7 +39,8 @@ class MainActivity : AppCompatActivity() {
         temperature = this.findViewById(R.id.temperature)
         model = AccuWeatherModelImpl()
         viewModel = ViewModelProviders.of(this).get(AccuWeatherViewModel::class.java)
-        viewModel.getLocationKey("-28.711964,32.169778",model)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocationUpdates()
         recyclerView = this.findViewById(R.id.WeatherInfoList)
         setAdapter()
         setLiveDataListeners()
@@ -49,7 +58,47 @@ class MainActivity : AppCompatActivity() {
         intent.data = Uri.parse(it)
         startActivity(intent)
     }
+    private fun getLocationUpdates() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 600000
+        locationRequest.fastestInterval = 600000
+        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    if (location != null) {
+                        locationStr = "${location.latitude},${location.longitude}"
+                        viewModel.getLocationKey(locationStr, model)
+                    }
+                }
+            }
+        }
+    }
 
+
+
+    //start location updates
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val permissionAccessCoarseLocationApproved = ActivityCompat
+            .checkSelfPermission(applicationContext!!, Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+
+        if (permissionAccessCoarseLocationApproved)
+            startLocationUpdates()
+        else
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+    }
     private fun setLiveDataListeners() {
         viewModel.locationInfoLiveData.observe(this,
             { t ->
